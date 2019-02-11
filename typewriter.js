798082e07d018ba556e4132eda59d9ad573679fb
Antonia <gintoni@gmx.at>
2019-02-11 20:10:47 +0100

    my first commit

--- typewriter.js
+++ typewriter.js
@@ -1,1 +1,15 @@
 "use strict";
+
+let i = 0;
+let txt = "This text is written one character at a time";
+let typesound = document.querySelector("#typekey1");
+
+function typewriter(){
+if (i < txt.length){
+    document.querySelector("#typewriter").innerHTML += txt.charAt(i);
+    i++;
+
+    setTimeout(typewriter, 200);
+}
+}
+typewriter();
